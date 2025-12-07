ee4b3dd73758f41038d4698733f7cfa3a90d8929
Mohsine Elkhatri <melkhatr@c1r8p1.1337.ma>
2025-12-07 13:43:26 +0100

    ft_printf 100/100

---
+++ ft_printf.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: melkhatr <melkhatr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/06 14:29:36 by melkhatr          #+#    #+#             */
+/*   Updated: 2024/11/28 16:52:42 by melkhatr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static int	format_print(va_list args, char c)
+{
+	int	count;
+	int	result;
+
+	result = 0;
+	count = 0;
+	if (c == 'c')
+		count = ft_putchar(va_arg(args, int));
+	else if (c == 's')
+		count = ft_putstr(va_arg(args, char *));
+	else if (c == 'd' || c == 'i')
+		count = ft_putnbr(va_arg(args, int));
+	else if (c == 'u')
+		count = ft_put_unsigned(va_arg(args, unsigned int));
+	else if (c == 'x' || c == 'X')
+		count = ft_put_hex(va_arg(args, unsigned int), c);
+	else if (c == 'p')
+		count = ft_put_pointer(va_arg(args, unsigned long long));
+	else if (c == '%')
+		count = ft_putchar('%');
+	result = result + count;
+	return (result);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+	int		len;
+	int		i;
+
+	i = 0;
+	len = 0;
+	va_start(args, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '\0')
+			{
+				va_end(args);
+				return (-1);
+			}
+			len += format_print(args, format[i]);
+		}
+		else
+			len += ft_putchar(format[i]);
+		i++;
+	}
+	va_end(args);
+	return (len);
+}
---
+++ ft_printf.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: melkhatr <melkhatr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/06 14:29:25 by melkhatr          #+#    #+#             */
+/*   Updated: 2024/11/28 11:40:58 by melkhatr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_printf(const char *format, ...);
+int	ft_putchar(char c);
+int	ft_putstr(char *str);
+int	ft_putnbr(int n);
+int	ft_put_unsigned(unsigned int n);
+int	ft_put_hex(unsigned long long num, char format);
+int	ft_put_pointer(unsigned long long ptr);
+
+#endif
